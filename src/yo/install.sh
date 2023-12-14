d05331683ea7522c80805f7471308ab71d40442e
Leonard M Reidy <leonard.reidy@gmail.com>
2023-12-14 18:29:27 +0000

    Add Yeoman user and configure env for Yeoman

--- src/yo/install.sh
+++ src/yo/install.sh
@@ -18,7 +18,26 @@ echo "The effective dev container containerUser's home directory is '$_CONTAINER
 cat > /usr/local/bin/yo \
 << EOF
 #!/bin/sh
-npm install --global --silent -y yo
+npm install --global --silent yo
+
+# Add a yeoman user because Yeoman freaks out and runs setuid(501).
+# This was because less technical people would run Yeoman as root and cause problems.
+# Setting uid to 501 here since it's already a random number being thrown around.
+# @see https://github.com/yeoman/yeoman.github.io/issues/282
+# @see https://github.com/cthulhu666/docker-yeoman/blob/master/Dockerfile
+adduser -D -u 501 yeoman && \
+  echo "yeoman ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+# Yeoman needs the use of a home directory for caching and certain config storage.
+export HOME=/home/yeoman
+
+mkdir /generated && chown yeoman:yeoman /generated
+
+# Always run as the yeoman user
+export USER=yeoman
+
 EOF
 
 chmod +x /usr/local/bin/yo
+
+
